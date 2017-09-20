d2dfde3d6af68fabdc025884fa3cac0f31eed389
seregpie@gmail.com <seregpie@gmail.com>
2017-09-20 17:58:23 +0200

    17.9.20

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # VueChart
+
+Vue wrapper for Chart.
+
+## dependencies
+
+- [Vue](https://github.com/vuejs/vue)
+- [Chart](https://github.com/chartjs/Chart.js)
+
+## setup
+
+Install the [package](https://www.npmjs.com/package/vuechart) via npm.
+
+```sh
+
+npm install vuechart
+
+```
+
+---
+
+Include the code in your page via a CDN.
+
+```html
+
+<script src="https://unpkg.com/vue"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.0/Chart.min.js"></script>
+<script src="https://unpkg.com/vuechart"></script>
+
+```
+
+## usage
+
+```html
+
+<vue-chart
+  type="bar"
+  :data="chartData"
+  :options="{scales: {yAxes: [{ticks: {beginAtZero: true}}]}}"
+  :update-config="{duration: 800, easing: 'easeOutBounce'}"
+></vue-chart>
+
+```
+
+## properties
+
+`type`
+
+...
+
+---
+
+`data`
+
+...
+
+---
+
+`options`
+
+...
+
+---
+
+`updateConfig`
+
+...
--- VueChart.js
+++ VueChart.js
@@ -1,6 +1,12 @@
-(function(Vue) {
+(function(factory) {
+	if (typeof module !== 'undefined' && typeof exports !== 'undefined' && this === exports) {
+		module.exports = factory(require('vue'), require('chart.js'));
+	} else {
+		this.Vue.component('VueChart', factory(this.Vue, this.Chart));
+	}
+}).call(this, function(Vue, Chart) {
 
-	Vue.component('VueChart', {
+	return {
 		render(createElement) {
 			return createElement('canvas');
 		},
@@ -9,33 +15,71 @@
 			type: String,
 			data: Object,
 			options: Object,
+			updateConfig: Object,
 		},
 
 		mounted() {
-			this.chart = new Chart(this.$el, {
-				type: this.type,
-				data: this.data,
-				options: this.options,
-			});
+			this.createChart();
 		},
 
 		watch: {
+			type(type) {
+				this.createChart();
+			},
+
 			data: {
 				handler(data) {
-					this.chart.data = data;
-					this.chart.update();
+					this.setChartData(data);
+					this.updateChart();
 				},
 				deep: true,
 			},
 
-			options: {
+			/*options: {
 				handler(options) {
-					this.chart.options = options;
-					this.chart.update();
+					this.setChartOptions(options);
+					this.updateChart();
 				},
 				deep: true,
+			},*/
+		},
+
+		methods: {
+			createChart() {
+				this.destroyChart();
+				if (this.$el) {
+					this.chart = new Chart(this.$el, {
+						type: this.type,
+						data: this.data,
+						options: this.options,
+					});
+				}
+			},
+
+			destroyChart() {
+				if (this.chart) {
+					this.chart.destroy();
+				}
+			},
+
+			setChartData(data) {
+				if (this.chart) {
+					this.chart.data = data;
+				}
+			},
+
+			/*setChartOptions(options) {
+				if (this.chart) {
+					this.chart.options = options;
+				}
+			},*/
+
+			updateChart() {
+				if (this.chart) {
+					this.chart.update(this.updateConfig);
+				}
 			},
 		},
-	});
+	};
 
-})(Vue);
+});
